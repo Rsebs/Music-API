22db0b06454bced2be3557342add46ed281833c5
Sebs <108592295+Rsebs@users.noreply.github.com>
2023-07-10 07:58:54 -0500

    Add files via upload

---
+++ src/test/java/net/ruiz/musicapi/MusicApiApplicationTests.java
@@ -0,0 +1,13 @@
+package net.ruiz.musicapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
