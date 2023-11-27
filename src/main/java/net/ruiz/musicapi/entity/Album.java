bb6165912a45ddf37be94fb99b80881db06d0541
Sebs <sebastianruizj2014@gmail.com>
2023-11-27 11:31:36 -0500

    DB
    Tabla de albums se crea

--- src/main/java/net/ruiz/musicapi/entity/Album.java
+++ src/main/java/net/ruiz/musicapi/entity/Album.java
@@ -2,6 +2,7 @@ package net.ruiz.musicapi.entity;
 
 import java.util.Date;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -13,6 +14,7 @@ import jakarta.persistence.Table;
 public class Album {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(unique = true, nullable = false)
     private Integer id;
     private String titulo;
     private Date lanzado;
